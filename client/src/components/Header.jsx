eaf2bb46dc0c7ee7a187c4cfe06e8c7de67085f8
German Popov <ProgDefv@gmail.com>
2023-07-18 17:09:32 +0400

    Add: header.jsx component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () =>{
+    return(
+        <header>
+            <nav>
+                <li><a href="http://vk.com">О нас</a></li>
+                <li><a href="http://vk.com">О нас</a></li>
+                <li><a href="http://vk.com">О нас</a></li>
+                <li><a href="http://vk.com">О нас</a></li>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
